96c113c3b0debf587647990e8996b1bb20e1eed0
DanielNasc <danielnasc15987@gmail.com>
2022-12-28 20:12:25 -0300

    Initialized: Meal history repository

---
+++ src/model/MealHistoryElement.ts
@@ -0,0 +1,21 @@
+import { Days } from "../utils/types";
+
+export class MealHistoryElement {
+    meal: "lunch" | "dinner";
+    day: Days;
+    user_id: string;
+    did_come: boolean;
+    respected_the_reservation: boolean;
+    is_fixed: boolean;
+    date: Date;
+     
+    constructor({ meal, day, user_id, did_come, respected_the_reservation, is_fixed }: Omit<MealHistoryElement, "date">) {
+        this.meal = meal;
+        this.day = day;
+        this.user_id = user_id;
+        this.did_come = did_come;
+        this.respected_the_reservation = respected_the_reservation;
+        this.is_fixed = is_fixed;
+        this.date = new Date();
+    }
+}
---
+++ src/repositories/implementations/MealHistoryRepository.ts
@@ -0,0 +1,43 @@
+import { MealHistoryElement } from "../../model/MealHistoryElement";
+import { Days } from "../../utils/types";
+import { IMealHistoryRepository } from "../IMealHistoryRepository";
+
+export class MealHistoryRepository implements IMealHistoryRepository {
+    private history: MealHistoryElement[];
+
+    private static INSTANCE: MealHistoryRepository;
+
+    private constructor() { 
+        this.history = [];
+    }
+
+    public static getInstance(): MealHistoryRepository {
+        if (!MealHistoryRepository.INSTANCE) {
+            MealHistoryRepository.INSTANCE = new MealHistoryRepository();
+        }
+
+        return MealHistoryRepository.INSTANCE;
+    }
+
+    addNewHistoryElement(props: Omit<MealHistoryElement, "date">): void {
+        const newHistoryElement = new MealHistoryElement(props);
+
+        this.history.push(newHistoryElement);
+    }
+
+    getHistoryByUserId(user_id: string): MealHistoryElement[] {
+        return this.history.filter(historyElement => historyElement.user_id === user_id);
+    }
+
+    getHistoryAfterDate(date: Date): MealHistoryElement[] {
+        return this.history.filter(historyElement => historyElement.date >= date);
+    }
+
+    getHistoryAfterDateByDayAndMeal(date: Date, day: Days, meal: "lunch" | "dinner"): MealHistoryElement[] {
+        return this.history.filter(historyElement => historyElement.date >= date && historyElement.day === day && historyElement.meal === meal);
+    }
+
+    getHistoryByDayAndMeal(day: Days, meal: "lunch" | "dinner"): MealHistoryElement[] {
+        return this.history.filter(historyElement => historyElement.day === day && historyElement.meal === meal);
+    }
+}
