061ef901deb87a09f32414d233c1dfcae9eb2f3f
DanielNasc <danielnasc15987@gmail.com>
2023-01-17 12:32:28 -0300

    handle app errors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import http from "http";
 import cors from "cors";
 
+import 'express-async-errors'
+
 import { router } from "./routes";
 import { SocketIoService } from "./services/SocketIo";
+
+import { AppError } from "./AppError";
 
 const PORT = process.env.PORT || 3000;
 
@@ -16,5 +20,17 @@ socketIO.setUpSocket(server)
 app.use(express.json());
 app.use(cors());
 app.use(router);
+
+app.use((err: Error | AppError, _: Request, res: Response, __: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      message: err.message
+    })
+  }
+
+  return res.status(500).json({
+    message: "Internal server error"
+  })
+})
 
 server.listen(PORT, () => console.log(`server running at port ${PORT}`));
--- src/useCases/createVeg/CreateVegUseCase.ts
+++ src/useCases/createVeg/CreateVegUseCase.ts
@@ -1,3 +1,4 @@
+import { AppError } from "../../AppError";
 import { MealReservationsRepository } from "../../repositories/implementations/postgres/MealReservationsRepository";
 import { VegsRepository } from "../../repositories/implementations/postgres/VegsRepository";
 import { getDayAndHour } from "../../utils/getDayAndHour";
@@ -5,43 +6,43 @@ import { getMeal } from "../../utils/getMeal";
 import { Days } from "../../utils/types";
 
 interface IRequestReservation {
-	day: Days;
-	meal: "lunch" | "dinner";
+  day: Days;
+  meal: "lunch" | "dinner";
 }
 
 interface ICreateVegProps {
-	card: number;
-	name: string;
-	schedule: IRequestReservation[];
+  card: number;
+  name: string;
+  schedule: IRequestReservation[];
 }
 
 export class CreateVegUseCase {
-	constructor(
-		private vegsRepository: VegsRepository,
-		private mealReservationsRepository: MealReservationsRepository
-	) { }
+  constructor(
+    private vegsRepository: VegsRepository,
+    private mealReservationsRepository: MealReservationsRepository
+  ) { }
 
-	async execute({ card, name, schedule }: ICreateVegProps): Promise<boolean> {
-		if (await this.vegsRepository.getIdByCard(card))
-			throw new Error("This card is already in use");
+  async execute({ card, name, schedule }: ICreateVegProps): Promise<boolean> {
+    if (await this.vegsRepository.getIdByCard(card))
+      throw new AppError("This card is already in use", 409);
 
-		const user_id = await this.vegsRepository.createVeg({ card, name }); // cria novo vegetariano
-		const { day, hour } = getDayAndHour();
-		const meal = getMeal(hour);
+    const user_id = await this.vegsRepository.createVeg({ card, name }); // cria novo vegetariano
+    const { day, hour } = getDayAndHour();
+    const meal = getMeal(hour);
 
-		let willComeToday = false;
+    let willComeToday = false;
 
-		for (const reservation of schedule) {
-			this.mealReservationsRepository.addNewReservation({
-				user_id,
-				meal: reservation.meal,
-				day: reservation.day
-			})
+    for (const reservation of schedule) {
+      this.mealReservationsRepository.addNewReservation({
+        user_id,
+        meal: reservation.meal,
+        day: reservation.day
+      })
 
-			if (reservation.day === day && reservation.meal === meal)
-				willComeToday = true;
-		}
+      if (reservation.day === day && reservation.meal === meal)
+        willComeToday = true;
+    }
 
-		return willComeToday
-	}
+    return willComeToday
+  }
 }
